5a59c5a9d698c99ac3861888e34807061cf8d045
billyxing <billy1211@gmail.com>
2015-10-30 16:52:47 +0800

    add user & downloadcode controller

---
+++ WebAPIOracleTest/Controllers/SELF_DOWNLOADCODEController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPIOracleTest.Models;
+
+namespace WebAPIOracleTest.Controllers
+{
+    [RoutePrefix("api/downcode")]
+    public class SELF_DOWNLOADCODEController : ApiController
+    {
+        private SelfDbEntities db = new SelfDbEntities();
+
+       
+
+        // GET: api/SELF_DOWNLOADCODE/5
+        [Route("{id:int}")]
+        [ResponseType(typeof(SELF_DOWNLOADCODE))]
+        public async Task<IHttpActionResult> GetSELF_DOWNLOADCODEByOrderId(long id)
+        {
+            SELF_DOWNLOADCODE sELF_DOWNLOADCODE = await db.SELF_DOWNLOADCODE.FindAsync(id);
+            if (sELF_DOWNLOADCODE == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(sELF_DOWNLOADCODE);
+        }
+
+        // PUT: api/SELF_DOWNLOADCODE/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutSELF_DOWNLOADCODE(long id, SELF_DOWNLOADCODE sELF_DOWNLOADCODE)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != sELF_DOWNLOADCODE.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(sELF_DOWNLOADCODE).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SELF_DOWNLOADCODEExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+        /*
+        // POST: api/SELF_DOWNLOADCODE
+        [ResponseType(typeof(SELF_DOWNLOADCODE))]
+        public async Task<IHttpActionResult> PostSELF_DOWNLOADCODE(SELF_DOWNLOADCODE sELF_DOWNLOADCODE)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.SELF_DOWNLOADCODE.Add(sELF_DOWNLOADCODE);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (SELF_DOWNLOADCODEExists(sELF_DOWNLOADCODE.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = sELF_DOWNLOADCODE.ID }, sELF_DOWNLOADCODE);
+        }
+
+        // DELETE: api/SELF_DOWNLOADCODE/5
+        [ResponseType(typeof(SELF_DOWNLOADCODE))]
+        public async Task<IHttpActionResult> DeleteSELF_DOWNLOADCODE(long id)
+        {
+            SELF_DOWNLOADCODE sELF_DOWNLOADCODE = await db.SELF_DOWNLOADCODE.FindAsync(id);
+            if (sELF_DOWNLOADCODE == null)
+            {
+                return NotFound();
+            }
+
+            db.SELF_DOWNLOADCODE.Remove(sELF_DOWNLOADCODE);
+            await db.SaveChangesAsync();
+
+            return Ok(sELF_DOWNLOADCODE);
+        }
+        */
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SELF_DOWNLOADCODEExists(long id)
+        {
+            return db.SELF_DOWNLOADCODE.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ WebAPIOracleTest/Controllers/SELF_USRSController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.Tracing;
+using WebAPIOracleTest.Models;
+
+namespace WebAPIOracleTest.Controllers
+{
+    [RoutePrefix("api/users")]
+    public class SELF_USRSController : ApiController
+    {
+        private SelfDbEntities db = new SelfDbEntities();
+
+        // GET: api/SELF_USRS
+        [Route("{pageNum:int}/{pageSize:int}")]
+        public IQueryable<SELF_USRS> GetSELF_USRSByPage(int pageNum, int pageSize)
+        {
+          
+
+            try
+            {
+
+                if (pageNum < 1) pageNum = 1;
+                if (pageSize < 1) pageSize = 40;
+
+                return db.SELF_USRS.OrderByDescending(p => p.ID).Skip(pageNum * pageSize).Take(pageSize);
+            }
+            catch (Exception ex)
+            {
+                Configuration.Services.GetTraceWriter().Error(Request, "SELF_USRSController_GetUSRSByPage", ex);
+                return null;
+            }
+        }
+
+        // GET: api/SELF_USRS/5
+        [Route("{id:int}")]
+        [ResponseType(typeof(SELF_USRS))]
+        public async Task<IHttpActionResult> GetSELF_USRS(long id)
+        {
+            SELF_USRS sELF_USRS = await db.SELF_USRS.FindAsync(id);
+            if (sELF_USRS == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(sELF_USRS);
+        }
+
+        // GET: api/SELF_USRS
+        [Route("CN/{strcn:string}")]
+        public IQueryable<SELF_USRS> GetSELF_USRSByCN(string cn)
+        {
+
+
+            try
+            {
+
+                if (string.IsNullOrEmpty(cn))
+                {
+                    Configuration.Services.GetTraceWriter().Error(Request, "SELF_USRSController_GetUSRSByPage", "CN项不能为空");
+                    return null;
+                }
+
+
+                return db.SELF_USRS.Where(p => p.UNAME.Contains(cn));
+            }
+            catch (Exception ex)
+            {
+                Configuration.Services.GetTraceWriter().Error(Request, "SELF_USRSController_GetUSRSByCN", ex);
+                return null;
+            }
+        }
+
+
+        // PUT: api/SELF_USRS/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutSELF_USRS(long id, SELF_USRS sELF_USRS)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != sELF_USRS.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(sELF_USRS).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SELF_USRSExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        /*
+        // POST: api/SELF_USRS
+        [ResponseType(typeof(SELF_USRS))]
+        public async Task<IHttpActionResult> PostSELF_USRS(SELF_USRS sELF_USRS)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.SELF_USRS.Add(sELF_USRS);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (SELF_USRSExists(sELF_USRS.ID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = sELF_USRS.ID }, sELF_USRS);
+        }
+
+        // DELETE: api/SELF_USRS/5
+        [ResponseType(typeof(SELF_USRS))]
+        public async Task<IHttpActionResult> DeleteSELF_USRS(long id)
+        {
+            SELF_USRS sELF_USRS = await db.SELF_USRS.FindAsync(id);
+            if (sELF_USRS == null)
+            {
+                return NotFound();
+            }
+
+            db.SELF_USRS.Remove(sELF_USRS);
+            await db.SaveChangesAsync();
+
+            return Ok(sELF_USRS);
+        }
+
+    */
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SELF_USRSExists(long id)
+        {
+            return db.SELF_USRS.Count(e => e.ID == id) > 0;
+        }
+    }
+}
