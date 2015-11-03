0dd19bad4ab1b7d4f63ea0c5b43ab3e9c03a6eb9
billyxing <billy1211@gmail.com>
2015-11-03 17:16:11 +0800

    修改orderinfocontroller，增加orderinfoDTO

--- WebAPIOracleTest/Controllers/SELF_ORDERINFOController.cs
+++ WebAPIOracleTest/Controllers/SELF_ORDERINFOController.cs
@@ -22,7 +22,7 @@ namespace WebAPIOracleTest.Controllers
         //分页查询
         [Route("{pageNum:int}/{pageSize:int}")]
         // GET: api/SELF_ORDERINFO
-        public IQueryable<SELF_ORDERINFO> GetSELF_ORDERINFOByPage(int pageNum,int pageSize)
+        public IQueryable<OrderInfoDTO> GetSELF_ORDERINFOByPage(int pageNum,int pageSize)
         {
             try
             {
@@ -30,7 +30,38 @@ namespace WebAPIOracleTest.Controllers
                 if (pageNum < 1) pageNum = 1;
                 if (pageSize < 1) pageSize = 40;
 
-                return db.SELF_ORDERINFO.OrderByDescending(p => p.ORDER_DATE).Skip(pageNum * pageSize).Take(pageSize);
+                var resultlist = (from _order in db.SELF_ORDERINFO
+                                 join _item in db.TBITEMs on _order.ITEMID equals _item.ITEMID
+                                 join _user in db.SELF_USRS on _order.USER_ID equals _user.ID
+                                 join _dcode in db.SELF_DOWNLOADCODE on _order.ID equals _dcode.ORDERID
+                                 orderby _order.ORDER_DATE descending
+                                 select new OrderInfoDTO
+                                 {
+                                     OrderId = _order.ID.ToString(),
+                                     OrderStatus = _order.ORDER_STATE,
+                                     OrderTime = _order.ORDER_DATE,
+                                     CompanyName = _user.UNAME,
+                                     ItemName = _item.ITEM,
+
+                                     PaymentType = _order.PAYMENT_TYPE.ToString(),
+                                     PaymentStatus = _order.PAYMENT_STATE,
+                                     PaymentTime = _order.PAYMENT_DATE,
+                                     PaymentId = _order.PAYMENT_ID,
+                                     Fee = _order.FEE.HasValue ? _order.FEE.Value : 0,
+
+                                     ShippingStatus = _order.MAIL_STATE,
+                                     ShippingAddress = _order.MAIL_ADDRESS,
+                                     ShippingType = _order.MAILTYPE.ToString(),
+                                     ShippingTime = _order.MAIL_DATE,
+                                     ShippingSN = _order.MAIL_SN,
+                                     ShippingPhone = _order.MAIL_TELNUM,
+                                     ShippingCode = _order.MAIL_POSTCODE,
+                                     DownloadCode = _dcode.DOWNLOADCODE,
+                                     Misc = _order.MISC
+                                 }).Skip(pageNum * pageSize).Take(pageSize);
+                return resultlist;
+
+                //return db.SELF_ORDERINFO.OrderByDescending(p => p.ORDER_DATE).Skip(pageNum * pageSize).Take(pageSize);
             }
             catch (Exception ex)
             {
@@ -42,13 +73,45 @@ namespace WebAPIOracleTest.Controllers
         //按订单号查询
         [Route("OrderId/{orderId:string}")]
         // GET: api/SELF_ORDERINFO
-        [ResponseType(typeof(SELF_ORDERINFO))]
+        [ResponseType(typeof(OrderInfoDTO))]
         public async Task<IHttpActionResult> GetSELF_ORDERINFO(string orderId)
         {
-            SELF_ORDERINFO sELF_ORDERINFO;
+            OrderInfoDTO sELF_ORDERINFO;
             try
             {
-                sELF_ORDERINFO = await db.SELF_ORDERINFO.SingleOrDefaultAsync(p => p.ORDER_ID == orderId);
+
+                sELF_ORDERINFO = await (from _order in db.SELF_ORDERINFO
+                 join _item in db.TBITEMs on _order.ITEMID equals _item.ITEMID
+                 join _user in db.SELF_USRS on _order.USER_ID equals _user.ID
+                 join _dcode in db.SELF_DOWNLOADCODE on _order.ID equals _dcode.ORDERID
+                 orderby _order.ORDER_DATE descending
+                 select new OrderInfoDTO
+                 {
+                     Id = _order.ID,
+                     OrderId = _order.ORDER_ID,
+                     OrderStatus = _order.ORDER_STATE,
+                     OrderTime = _order.ORDER_DATE,
+                     CompanyName = _user.UNAME,
+                     ItemName = _item.ITEM,
+
+                     PaymentType = _order.PAYMENT_TYPE.ToString(),
+                     PaymentStatus = _order.PAYMENT_STATE,
+                     PaymentTime = _order.PAYMENT_DATE,
+                     PaymentId = _order.PAYMENT_ID,
+                     Fee = _order.FEE.HasValue ? _order.FEE.Value : 0,
+
+                     ShippingStatus = _order.MAIL_STATE,
+                     ShippingAddress = _order.MAIL_ADDRESS,
+                     ShippingType = _order.MAILTYPE.ToString(),
+                     ShippingTime = _order.MAIL_DATE,
+                     ShippingSN = _order.MAIL_SN,
+                     ShippingPhone = _order.MAIL_TELNUM,
+                     ShippingCode = _order.MAIL_POSTCODE,
+                     DownloadCode = _dcode.DOWNLOADCODE,
+                     Misc = _order.MISC
+                 }).SingleOrDefaultAsync();
+                
+               // sELF_ORDERINFO = await db.SELF_ORDERINFO.SingleOrDefaultAsync(p => p.ORDER_ID == orderId);
             }
             catch (Exception ex)
             {
@@ -70,10 +133,58 @@ namespace WebAPIOracleTest.Controllers
         //按id查询
         // GET: api/SELF_ORDERINFO/5
         [Route("{id:int}")]
-        [ResponseType(typeof(SELF_ORDERINFO))]
+        [ResponseType(typeof(OrderInfoDTO))]
         public async Task<IHttpActionResult> GetSELF_ORDERINFO(long id)
         {
-            SELF_ORDERINFO sELF_ORDERINFO = await db.SELF_ORDERINFO.FindAsync(id);
+            //SELF_ORDERINFO sELF_ORDERINFO = await db.SELF_ORDERINFO.FindAsync(id);
+
+            OrderInfoDTO sELF_ORDERINFO;
+            try
+            {
+
+                sELF_ORDERINFO = await (from _order in db.SELF_ORDERINFO
+                                        join _item in db.TBITEMs on _order.ITEMID equals _item.ITEMID
+                                        join _user in db.SELF_USRS on _order.USER_ID equals _user.ID
+                                        join _dcode in db.SELF_DOWNLOADCODE on _order.ID equals _dcode.ORDERID
+                                        where _order.ID == id
+                                        orderby _order.ORDER_DATE descending
+                                        select new OrderInfoDTO
+                                        {
+                                            Id = _order.ID,
+                                            OrderId = _order.ORDER_ID,
+                                            OrderStatus = _order.ORDER_STATE,
+                                            OrderTime = _order.ORDER_DATE,
+                                            CompanyName = _user.UNAME,
+                                            ItemName = _item.ITEM,
+
+                                            PaymentType = _order.PAYMENT_TYPE.ToString(),
+                                            PaymentStatus = _order.PAYMENT_STATE,
+                                            PaymentTime = _order.PAYMENT_DATE,
+                                            PaymentId = _order.PAYMENT_ID,
+                                            Fee = _order.FEE.HasValue ? _order.FEE.Value : 0,
+
+                                            ShippingStatus = _order.MAIL_STATE,
+                                            ShippingAddress = _order.MAIL_ADDRESS,
+                                            ShippingType = _order.MAILTYPE.ToString(),
+                                            ShippingTime = _order.MAIL_DATE,
+                                            ShippingSN = _order.MAIL_SN,
+                                            ShippingPhone =_order.MAIL_TELNUM,
+                                            ShippingCode = _order.MAIL_POSTCODE,
+                                            DownloadCode =_dcode.DOWNLOADCODE,
+                                            Misc = _order.MISC
+                                        }).FirstOrDefaultAsync();
+
+                // sELF_ORDERINFO = await db.SELF_ORDERINFO.SingleOrDefaultAsync(p => p.ORDER_ID == orderId);
+            }
+            catch (Exception ex)
+            {
+                sELF_ORDERINFO = null;
+                Configuration.Services.GetTraceWriter().Error(Request, "SELF_ORDERINFO_GetOrderByOrderId", ex);
+            }
+
+
+
+
             if (sELF_ORDERINFO == null)
             {
                 return NotFound();
@@ -86,8 +197,24 @@ namespace WebAPIOracleTest.Controllers
         // PUT: api/SELF_ORDERINFO/5
         [Route("{id:int}")]
         [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutSELF_ORDERINFO(long id, SELF_ORDERINFO sELF_ORDERINFO)
+        public async Task<IHttpActionResult> PutSELF_ORDERINFO(long id, OrderInfoDTO sELF_ORDERINFODTO)
         {
+
+            SELF_ORDERINFO sELF_ORDERINFO =await db.SELF_ORDERINFO.FirstOrDefaultAsync(p => p.ID == sELF_ORDERINFODTO.Id);
+
+            if(sELF_ORDERINFO==null)
+            {
+                return NotFound();
+            }
+
+            sELF_ORDERINFO.MAIL_SN =sELF_ORDERINFODTO.ShippingSN;
+            sELF_ORDERINFO.MAIL_STATE =sELF_ORDERINFODTO.ShippingStatus;
+            sELF_ORDERINFO.MAIL_DATE = sELF_ORDERINFODTO.ShippingTime;
+            sELF_ORDERINFO.MISC = sELF_ORDERINFODTO.Misc;
+
+            sELF_ORDERINFO.ORDER_STATE = sELF_ORDERINFODTO.OrderStatus;
+
+
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
---
+++ WebAPIOracleTest/Models/OrderInfoDTO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIOracleTest.Models
+{
+    public class OrderInfoDTO
+    {
+        public long Id { get; set; }//序号
+        public string OrderId { get; set; } //订单号
+        
+        public string OrderStatus { get; set; } //订单状态
+        public string CompanyName { get; set; } //客户（或单位）名称
+        public string ItemName { get; set; } //项目名称
+       
+        public DateTime? OrderTime { get; set; } //订单创建时间
+
+
+
+        public string PaymentType { get; set; } //支付方式
+        public string PaymentStatus { get; set; } //支付状态
+        public DateTime? PaymentTime { get; set; } //支付时间
+        public string PaymentId { get; set; } //支付流水号
+        public decimal Fee { get; set; } //金额
+
+        public string ShippingType { get; set; } //邮寄类型
+        public string ShippingStatus { get; set; } //邮寄状态
+        public DateTime? ShippingTime { get; set; } //邮寄时间
+        public string ShippingAddress { get; set; } //邮寄地址
+        public string ShippingSN { get; set; }//快递单号
+        public string ShippingPhone { get; set; }//收件人电话
+        public string ShippingCode { get; set; }//邮编
+        public string DownloadCode { get; set; }//下载码
+        public string Misc { get; set; }//备注
+
+
+    }
+}
